12c7ef4f35b30b3c274b1ad77aadcb1d435526aa
GauravWalia19 <gauravwalia019@gmail.com>
2020-06-18 00:08:12 +0530

    added github option

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -3,19 +3,27 @@ import '../styles/Header.css';
 
 const Header = () => {
     return (
-        <div className="outerHeader">
-            <div className="header">
-                <a href="/" className="homeLink">
-                    <i className="fas fa-book"></i>
-                    <span>Free Algorithm Books</span>
-                </a>
-                <ul className="navLinkList">
-                    <li className="navLinks"><a href="/#secondPage">Library</a></li>
-                    <li className="navLinks"><a href="/#thirdPage">Community</a></li>
-                    <li className="navLinks">v1.0.0</li>
-                </ul>
+        <React.Fragment>
+            <div className="outerHeader">
+                <div className="header">
+                    <a href="/" className="homeLink">
+                        <i className="fas fa-book"></i>
+                        <span>Free Algorithm Books</span>
+                    </a>
+                    <ul className="navLinkList">
+                        <li className="navLinks"><a href="/#secondPage">Library</a></li>
+                        <li className="navLinks"><a href="/#thirdPage">Community</a></li>
+                        <li className="navLinks">v1.0.0</li>
+                    </ul>
+                </div>
+                <div className="forkOption">
+                    <a href="https://github.com/GauravWalia19/Free-Algorithms-Books">
+                        Fork on github
+                    </a>
+                </div>
             </div>
-        </div>
+            
+        </React.Fragment>
     )
 }
 
